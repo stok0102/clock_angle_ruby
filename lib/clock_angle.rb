37082368973d6a57ccb7548fa2217dd023b6c917
Caleb Stokka and Chip Carnes <caleb+chip@>
2016-06-27 15:10:03 -0700

    passes minute spec

--- lib/clock_angle.rb
+++ lib/clock_angle.rb
@@ -3,7 +3,7 @@ require('pry')
 class Array
   define_method(:clock_angle) do
     minute_degree = self[1]*6
-    hour_degree = self[0]*30 + self[1]/2
+    hour_degree = self[0]*30 + self[1].to_f()/2
     angle = hour_degree - minute_degree
     # binding.pry
   end
--- spec/clock_angle_spec.rb
+++ spec/clock_angle_spec.rb
@@ -5,7 +5,10 @@ describe('Array#clock_angle') do
   it("is 0 at 12:00") do
     expect([0,0].clock_angle()).to(eq(0))
   end
-  # it("is 0 at 12:00") do
-  #   expect([0,0].clock_angle()).to(eq(0))
-  # end
+  it("is 180 at 6:00") do
+    expect([6,0].clock_angle()).to(eq(180))
+  end
+  it("is 24.5 at 1:01") do
+    expect([1,1].clock_angle()).to(eq(24.5))
+  end
 end
